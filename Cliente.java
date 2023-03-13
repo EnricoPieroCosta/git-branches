e4ad638eedb02fcf66c769400b05da00d353e964
Enrico Piero Costa <enrico.santos@germinare.org.br>
2023-03-13 17:48:46 -0300

    adicionando idea 3

--- Cliente.java
+++ Cliente.java
@@ -1,7 +1,11 @@
 public class Cliente {
+<<<<<<< HEAD
     private String nome;
     private String cpf;
     private int acencia;
     private double saldo;
     
+=======
+    private string tipoConta
+>>>>>>> 5c4dadd (Adicionando tipoConta no arquivo Cliente.java)
 }
