6c4e4f67097634ea873e834a0800e9619f1a96a5
Enrico Piero Costa <enrico.santos@germinare.org.br>
2023-03-20 16:30:26 -0300

    adicionando a correção

--- Cliente.java
+++ Cliente.java
@@ -1,15 +1,6 @@
 public class Cliente {
-<<<<<<< HEAD
-<<<<<<< HEAD
     private String nome;
     private String cpf;
     private int acencia;
     private double saldo;
-    
-=======
-    private string tipoConta
->>>>>>> 5c4dadd (Adicionando tipoConta no arquivo Cliente.java)
-=======
-    
->>>>>>> a77c967 (adicionando idea)
 }
